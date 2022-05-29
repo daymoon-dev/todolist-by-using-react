51af451a541b1471873ac4c5e8a83f0de01c29eb
daymoon-dev <icn.daymoon@gmail.com>
2022-05-29 16:28:22 +0900

    라우터 추가 및 적용

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import { TodoTemplate } from "./components/TodoTemplate";
 import { Main } from "./components/Titles";
 import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
-import { useFetch } from "./hooks/useFetch";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { useState, useEffect } from "react";
 
 function App() {
-  const todoItem = useFetch("http://localhost:3001/todoItems");
+  const [todoItems, setTodoItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/todoItems`)
+      .then((response) => response.json())
+      .then((data) => setTodoItems(data));
+  }, []);
 
   return (
-    <>
-      <TodoTemplate>
-        <Main title="TODOLIST" />
-        <TodoForm todoItem={todoItem} />
-        <TodoList todoItem={todoItem} />
-      </TodoTemplate>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <TodoTemplate>
+            <Main title="TODOLIST" />
+            <TodoForm todoItems={todoItems} setTodoItems={setTodoItems} />
+            <TodoList todoItems={todoItems} setTodoItems={setTodoItems} />
+          </TodoTemplate>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export function Login() {
+    return (
+        <>
+            <h1>로그인 페이지</h1>
+            <Link to="/">테스트</Link>
+        </>
+    )
+}
